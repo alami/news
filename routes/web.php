72c03db8a65539c53ee89fa681c1fbaebf95df15
alami <alami@km.ru>
2022-12-29 14:56:01 +0500

    ch04.02 new dashboard on public/backend/assets and resource/view/admin/..

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Demo\DemoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,10 +17,15 @@ use App\Http\Controllers\Demo\DemoController;
 Route::get('/', function () {
     return view('welcome');
 });
-/*Route::get('/about', function () {
-    echo "This is about page";
-});*/
-//Route::get('/about', [DemoController::class, 'Index']);
-Route::controller(DemoController::class)->group(function (){
-    Route::get('/about', 'Index') -> name('about.page') -> middleware('check');
+
+Route::get('/dashboard', function () {
+    return view('admin.index');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
