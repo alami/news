95c803106c158b2cd339a147f1e6d63b2e84a903
alami <alami@km.ru>
2023-01-03 19:43:55 +0500

    ch04.17 pswd change

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -43,5 +44,26 @@ class AdminController extends Controller
         $notification = array('message' => 'Admin Profile Update Successfully',
              'alert-type' => 'success');
         return redirect()->route('admin.profile')->with($notification);
+    }
+    public function ChangePassword () {
+        return view('admin.admin_change_password');
+    }
+    public function UpdatePassword (Request $request) {
+        $validateData = $request->validate([
+            'oldpassword'=>'required',
+            'newpassword'=>'required',
+            'confirm_password'=>'required|same:newpassword',
+        ]);
+        $hashedPassword = Auth::user()->password;
+        if (Hash::check($request->oldpassword,$hashedPassword)) {
+            $users = User::find(Auth::id());
+            $users->password = bcrypt($request->newpassword);
+            $users->save();
+            session()->flash('message', 'Password Update Successfully');
+            return redirect()->back();
+        } else {
+            session()->flash('message', 'Old Password id not match');
+            return redirect()->back();
+        }
     }
 }
